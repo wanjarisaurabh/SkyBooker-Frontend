a182f8ae9628f95874835a0ea01c5d784ab06058
saurabh wanjari <wanjarisaurabh10@gmail.com>
2026-05-19 15:44:28 +0530

    [saurabh wanjari] Add . Added missing environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.skybooker.com', // Replace with your production API URL
+  authServiceUrl: 'https://auth.skybooker.com', // Replace with your production Auth URL
+  razorpayKeyId: 'rzp_live_XXXXXXXXXXXXXX', // Replace with your production Razorpay Key
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  authServiceUrl: 'http://localhost:8081',
+  razorpayKeyId: 'rzp_test_SjCut2LS1EHvsP',
+};
